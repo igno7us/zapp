f3a9d0c71c013945605344b4783dbf242435f7fb
igno7us <igno7us@gmail.com>
2024-11-24 20:24:29 -0300

    app

--- screens/firebaseConfig.js
+++ screens/firebaseConfig.js
@@ -1,9 +1,19 @@
-import appfirebase from '../credenciales';
 import { getFirestore } from 'firebase/firestore'; 
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { initializeApp } from "firebase/app";
 
 
+const firebaseConfig = {
+  apiKey: "AIzaSyBse1z-uLNBBrMMXIWvcuwMDH2CNsNv5OM",
+  authDomain: "proyectoz-a0a28.firebaseapp.com",
+  projectId: "proyectoz-a0a28",
+  storageBucket: "proyectoz-a0a28.appspot.com",
+  messagingSenderId: "715754604208",
+  appId: "1:715754604208:web:04765bd9817c5fe0d25d75",
+  measurementId: "G-6ZLCDE19TK"
+};
+const appfirebase = initializeApp(firebaseConfig);
 
 const auth = initializeAuth(appfirebase, {
   persistence: getReactNativePersistence(AsyncStorage),
